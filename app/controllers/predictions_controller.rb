d28faece3883d9b89174b6489ebb894aaa65cf38
github username <github email address>
2019-09-24 14:10:53 -0400

    wired predict form to persist to backend when user creates prediction

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -1,2 +1,10 @@
 class PredictionsController < ApplicationController
+
+    def create
+        stock = Equity.create(symbol: params[:newStock][:ticker])
+        prediction = Prediction.create(user_id: current_user.id, equity_id: stock.id, price: params[:info][:price].to_i, date: params[:info][:date])
+        render json: {message: "You have successfulyl created a prediction!"}
+    end 
+
+
 end
